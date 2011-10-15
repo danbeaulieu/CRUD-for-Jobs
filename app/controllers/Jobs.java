8e07575fd0eb46918a7987a4c85b8c7c0cf57823
Dan Beaulieu <danjacob.beaulieu@gmail.com>
2011-10-15 17:20:12 -0400

    Add in ability to run the job

--- app/controllers/Jobs.java
+++ app/controllers/Jobs.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import org.apache.commons.lang.StringUtils;
+
 import models.JobInfo;
+import play.Logger;
+import play.exceptions.UnexpectedException;
 import play.jobs.Job;
 import play.jobs.JobsPlugin;
 import play.mvc.Before;
@@ -25,5 +29,43 @@ public class Jobs extends CRUD {
 	public static void create() throws Exception {
 		
 		render("CRUD/index.html");
+	}
+	
+	/*
+	 * Lots of code cribbed from play-jobs module
+	 * 
+	 */
+	public static void runNow(String jobClass) {
+		Logger.info(String.format("Job Class: %s, Number of Concurrent Instances: %s", jobClass, 1));
+		if (StringUtils.isBlank(jobClass)) {
+			throw new RuntimeException("Invalid Job Class Name");
+		}
+		
+		try {
+			Class clazz = Class.forName(jobClass);
+			if (clazz == null) {
+				throw new RuntimeException("Invalid Job Class: " + jobClass);
+			}
+			// Create Instance
+			Object o = clazz.newInstance();
+
+			// Check Instance Type
+			if ((o instanceof Job) == false) {
+				throw new RuntimeException("Invalid Class Instance: " + o);
+			}
+
+			// Log Debug
+			Logger.info("Starting Job: %s", o);
+
+			// Fire Job
+			Job job = (Job) o;
+			job.now();
+			Logger.info("Started Job: %s", job);
+		} catch (Throwable t) {
+			Logger.error(t, "Error running job");
+			throw new UnexpectedException(String.format("Couldn't run job: %s", jobClass));
+		}
+		
+		flash.success("Ran Job: %s", jobClass);
 	}
 }
